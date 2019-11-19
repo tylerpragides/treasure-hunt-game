6d38bd420774ea658e34fc7188674fd49e7be83f
tylerpragides <32971540+tylerpragides@users.noreply.github.com>
2019-11-19 13:55:27 -0800

    j

--- 1-objects-classes/sketch.js
+++ 1-objects-classes/sketch.js
@@ -1,77 +1,149 @@
-//create a variable to hold one ball
-// let b;
-// let anotherPlanet;
-// let planet3;
+
+let treasures = [];
+let test123;
+let freeze = [];
 
 function setup() {
-  createCanvas(800, 400);
-  b = new Planet(0, 100,"red",20,2,2); //make a new ball from the Planet class and call it b.
-  anotherPlanet = new Planet(200,20,"green",40,2,1);
-  planet3 = new Planet(800,400,"blue",30,-1,-1.5);
-  john = new Astronaut(400,400,20,0.25,-0.5)
-}
+  test123 = 'red'
+  createCanvas(600, 600);
+  background(255,225,185);
+  player1 = new Avatar(random(50,550),random(50,550),4,0,'red',1)
+  player2 = new Avatar(random(50,550),random(50,550),4,0,'blue',2)
 
+  for(var i = 0; i < 2; i++){
+    let wow = new Treasure()
+    treasures.push(wow)
+  }
+  for(var p = 0; p <= 1; p++){
+    freeze.push(treasures[int(random(0,treasures.length))])
+  }
+}
 
 function draw(){
-	background(100);
-    b.drawPlanet(); //draw the ball called b (go look in the Planet class for the drawPlanet function)
-    b.movePlanet(); //move the ball called b (go look in the Planet class for the movePlanet function)
-    anotherPlanet.drawPlanet();
-    anotherPlanet.movePlanet();
-    planet3.drawPlanet();
-    planet3.movePlanet();
-    john.drawAstronaut();
-    john.moveAstronaut();
-
+  text(key,33,65)
+  background(255,225,185);
+  for(var i = 0; i < treasures.length; i++){
+  treasures[i].drawTreasure();
 }
 
-
-//ball class from which to create new balls with similar properties.
-class Planet {
-
-	constructor(x,y,color,radius,xspeed,yspeed){ //every ball needs an x value and a y value
-		    this.x = x;
-    		this.y = y;
-        this.color= color;
-        this.radius = radius;
-        this.xspeed = xspeed;
-        this.yspeed = yspeed;
-	}
-	drawPlanet(){  // draw a ball on the screen at x,y
-    		stroke(0);
-    		fill(this.color);
-		    ellipse(this.x,this.y,this.radius,this.radius);
-	}
-	movePlanet(){ //update the location of the ball, so it moves across the screen
-		this.x = this.x+this.xspeed;
-		this.y = this.y+this.yspeed;
-	}
+  player1.drawMe()
+  player1.moveMe()
+  player2.drawMe()
+  player2.moveMe()
+  player1.collideMe()
+  player2.collideMe()
+  player1.openItem()
+  player2.openItem()
 }
 
-class Astronaut {
+class Avatar {
 
-	constructor(x,y,radius,xspeed,yspeed){ //every ball needs an x value and a y value
-		    this.x = x;
-    		this.y = y;
-        this.radius = radius;
-        this.xspeed = xspeed;
-        this.yspeed = yspeed;
+	constructor(x,y, speed, score, color, player){
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.score = score;
+    this.color = color;
+    this.player = player;
 	}
-	drawAstronaut(){  // draw a ball on the screen at x,y
-    		stroke(0);
-        noFill()
-		    ellipse(this.x,this.y,this.radius,this.radius);
-        fill(0,0,0)
-        ellipse(this.x - 5,this.y - 2,this.radius - 18, this.radius - 18)
-        fill(0,0,0)
-        ellipse(this.x + 5,this.y - 2,this.radius - 18, this.radius - 18)
-        noFill()
-        arc(this.x,this.y-2,this.radius-5,this.radius-5,PI*2/10,PI*8/10)
-        noFill()
-        rect(this.x - 10,this.y + 10,this.radius,this.radius)
+
+	drawMe(){
+    noStroke()
+    fill(this.color)
+    ellipse(this.x,this.y,20,20)
+  }
+
+	moveMe(){
+    if (keyIsDown(UP_ARROW) && this.player == 2) {
+       this.y -= this.speed;
+    }
+
+    if (keyIsDown(DOWN_ARROW) && this.player == 2) {
+        this.y += this.speed;
+    }
+
+    if (keyIsDown(LEFT_ARROW) && this.player == 2) {
+      this.x -= this.speed;
+    }
+
+    if (keyIsDown(RIGHT_ARROW) && this.player == 2) {
+      this.x += this.speed;
+    }
+    if (keyIsDown(87) && this.player == 1) {
+       this.y -= this.speed;
+    }
+
+    if (keyIsDown(83) && this.player == 1) {
+        this.y += this.speed;
+    }
+
+    if (keyIsDown(65) && this.player == 1) {
+      this.x -= this.speed;
+    }
+
+    if (keyIsDown(68) && this.player == 1) {
+      this.x += this.speed;
+    }
 	}
-	moveAstronaut(){ //update the location of the ball, so it moves across the screen
-		this.x = this.x+this.xspeed;
-		this.y = this.y+this.yspeed;
-	}
+
+  collideMe(){
+    if(this.x + 10 >= width){
+      this.x = this.x - this.speed;
+    }
+    if(this.x - 10 <= 0){
+      this.x = this.x + this.speed;
+    }
+    if(this.y - 10 <= 0){
+      this.y = this.y + this.speed;
+    }
+    if(this.y + 10 >= height){
+      this.y = this.y - this.speed;
+    }
+  }
+
+  openItem(){
+
+  for(let i = 0; i<treasures.length; i++){
+    if(keyIsDown(81) == true && this.player == 1 && player1.x <= treasures[i].x + 10 && player1.x >= treasures[i].x -10 && player1.y <= treasures[i].y + 10 && player1.y >= treasures[i].y){
+      player2.speed = 0;
+      test123 = 'green'
+    }
+    if(keyIsDown(81) == false && this.player == 1){
+      player2.speed = 4;
+    }
+    if(player1.x >= treasures[i].x + 10 || player1.x <= treasures[i].x -10 || player1.y >= treasures[i].y + 10 || player1.y <= treasures[i].y){
+      if(this.player == 1){
+        player2.speed = 4;
+      }
+    }
+    if(keyIsDown(18) == true && this.player == 2 && player2.x <= treasures[i].x + 10 && player2.x >= treasures[i].x -10 && player2.y <= treasures[i].y + 10 && player2.y >= treasures[i].y){
+      player1.speed = 0;
+      test123 = 'green'
+    }
+    if(keyIsDown(18) == false && this.player == 2){
+      player1.speed = 4;
+    }
+    if(player2.x >= treasures[i].x + 10 || player2.x <= treasures[i].x -10 || player2.y >= treasures[i].y + 10 || player2.y <= treasures[i].y){
+      if(this.player == 2){
+        player1.speed = 4;
+      }
+    }
+  }
+}
+}
+
+class Treasure {
+  constructor(){
+    this.x = random(20,580);
+    this.y = random(20,580);
+
+  }
+
+  drawTreasure(){
+    stroke(test123)
+    strokeWeight(2)
+    fill(255,0,0)
+    line(this.x - 10, this.y - 10, this.x + 10, this.y + 10)
+    line(this.x - 10, this.y + 10, this.x + 10, this.y - 10)
+  }
 }
--- game/treasure.js
+++ game/treasure.js
@@ -1,5 +1,6 @@
 
 let treasures = [];
+let freeze = [];
 
 
 function setup() {
@@ -8,9 +9,13 @@ function setup() {
   player1 = new Avatar(random(50,550),random(50,550),4,0,'red',1)
   player2 = new Avatar(random(50,550),random(50,550),4,0,'blue',2)
 
-  for(var i = 0; i < 100; i++){
+  for(var i = 0; i < 10; i++){
     let wow = new Treasure()
     treasures.push(wow)
+  }
+
+  for(var x = 0; x < 3; x++){
+    freeze.push(treasures[int(random(0,treasures.length))])
   }
 }
 
@@ -98,14 +103,14 @@ class Avatar {
 
   openItem(){
 
-  for(let i = 0; i<treasures.length; i++){
-    if(keyIsDown(81) == true && this.player == 1 && player1.x <= treasures[i].x + 10 && player1.x >= treasures[i].x -10 && player1.y <= treasures[i].y + 10 && player1.y >= treasures[i].y){
+  for(let i = 0; i<freeze.length; i++){
+    if(keyIsDown(81) == true && this.player == 1 && player1.x <= freeze[i].x + 10 && player1.x >= freeze[i].x -10 && player1.y <= freeze[i].y + 10 && player1.y >= freeze[i].y){
       player2.speed = 0;
     }
     if(keyIsDown(81) == false && this.player == 1){
       player2.speed = 4;
     }
-    if(keyIsDown(18) == true && this.player == 2 && player2.x <= treasures[i].x + 10 && player2.x >= treasures[i].x -10 && player2.y <= treasures[i].y + 10 && player2.y >= treasures[i].y){
+    if(keyIsDown(18) == true && this.player == 2 && player2.x <= freeze[i].x + 10 && player2.x >= freeze[i].x -10 && player2.y <= freeze[i].y + 10 && player2.y >= freeze[i].y){
       player1.speed = 0;
     }
     if(keyIsDown(18) == false && this.player == 2){
@@ -128,5 +133,11 @@ class Treasure {
     fill(255,0,0)
     line(this.x - 10, this.y - 10, this.x + 10, this.y + 10)
     line(this.x - 10, this.y + 10, this.x + 10, this.y - 10)
+  }
+}
+
+class Items {
+  constructor(){
+    this.x = x
   }
 }

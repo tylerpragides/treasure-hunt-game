4d78fca1ba4d6808369e3cd09aca8200ba587b61
tylerpragides <32971540+tylerpragides@users.noreply.github.com>
2019-11-18 18:13:25 -0800

    yes

--- game/treasure.js
+++ game/treasure.js
@@ -1,18 +1,34 @@
 
+let treasures = [];
+
+
 function setup() {
   createCanvas(600, 600);
+  background(255,225,185);
   player1 = new Avatar(random(50,550),random(50,550),4,0,'red',1)
   player2 = new Avatar(random(50,550),random(50,550),4,0,'blue',2)
+
+  for(var i = 0; i < 100; i++){
+    let wow = new Treasure()
+    treasures.push(wow)
+  }
 }
 
 function draw(){
-  background(255,195,55);
+  text(key,33,65)
+  background(255,225,185);
+  for(var i = 0; i < treasures.length; i++){
+  treasures[i].drawTreasure();
+}
+
   player1.drawMe()
   player1.moveMe()
   player2.drawMe()
   player2.moveMe()
   player1.collideMe()
   player2.collideMe()
+  player1.openItem()
+  player2.openItem()
 }
 
 class Avatar {
@@ -79,12 +95,38 @@ class Avatar {
       this.y = this.y - this.speed;
     }
   }
+
+  openItem(){
+
+  for(let i = 0; i<treasures.length; i++){
+    if(keyIsDown(81) == true && this.player == 1 && player1.x <= treasures[i].x + 10 && player1.x >= treasures[i].x -10 && player1.y <= treasures[i].y + 10 && player1.y >= treasures[i].y){
+      player2.speed = 0;
+    }
+    if(keyIsDown(81) == false && this.player == 1){
+      player2.speed = 4;
+    }
+    if(keyIsDown(18) == true && this.player == 2 && player2.x <= treasures[i].x + 10 && player2.x >= treasures[i].x -10 && player2.y <= treasures[i].y + 10 && player2.y >= treasures[i].y){
+      player1.speed = 0;
+    }
+    if(keyIsDown(18) == false && this.player == 2){
+      player1.speed = 4;
+    }
+  }
+}
 }
 
 class Treasure {
-  constructor(x,y,item){
-    this.x = x;
-    this.y = y;
-    this.item = item;
+  constructor(){
+    this.x = random(20,580);
+    this.y = random(20,580);
+
+  }
+
+  drawTreasure(){
+    stroke(200,0,0)
+    strokeWeight(2)
+    fill(255,0,0)
+    line(this.x - 10, this.y - 10, this.x + 10, this.y + 10)
+    line(this.x - 10, this.y + 10, this.x + 10, this.y - 10)
   }
 }

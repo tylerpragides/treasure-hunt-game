441751dab8c96457383f3aa576c571f2b30c8f44
tylerpragides <32971540+tylerpragides@users.noreply.github.com>
2019-11-25 09:46:03 -0800

    o
    made another copy

--- 1-objects-classes/sketch copy.js
+++ 1-objects-classes/sketch copy.js
@@ -1,13 +1,17 @@
 
 //maybe make freeze only store the number within array treasures that corresponds to the freeze items
 //draw treasures after openitem
+//can't put "this" in stroke to change color (line169) unless it is in another function
+
+// 11-25 when the player presses Q or ALT on an spot, it disappears, except if it is on "special" spot everything disappears
 
 let treasures = [];
-let test123;
-let freeze = [];
+// let test123;
+// let freeze = [];
+// let freezes;
 
 function setup() {
-  test123 = 'red'
+  // test123 = 'red'
   createCanvas(600, 600);
   background(255,225,185);
   player1 = new Avatar(random(50,550),random(50,550),3,0,'red',1)
@@ -17,20 +21,25 @@ function setup() {
     let wow = new Treasure()
     treasures.push(wow)
   }
-  for(var p = 0; p < 1; p++){
-    freeze.push(treasures[int(random(0,treasures.length))])
-    console.log(freeze)
-  }
+  let special = new Treasure()
+  treasures.push(special)
+  console.log(special)
+
+  // for(var p = 0; p < 1; p++){
+  //   freeze.push(treasures[int(random(0,treasures.length))])
+  //   console.log(freeze)
+  // }
 }
 
 function draw(){
   background(255,225,185);
   player1.openItem()
   player2.openItem()
-  
+
   for(var i = 0; i < treasures.length; i++){
-    stroke(test123)
+    // stroke(test123)
     treasures[i].drawTreasure();
+    // treasures[i].eraseTreasure();
 }
 
   player1.drawMe()
@@ -117,45 +126,58 @@ class Avatar {
 
   openItem(){
 
-
-    if(keyIsDown(81) == true && this.player == 1 && player1.x <= freeze[0].x + 10 && player1.x >= freeze[0].x -10 && player1.y <= freeze[0].y + 10 && player1.y >= freeze[0].y - 10){
+for(var i = 0; i < treasures.length; i++){
+    if(keyIsDown(81) == true && this.player == 1 && player1.x <= treasures[i].x + 10 && player1.x >= treasures[i].x -10 && player1.y <= treasures[i].y + 10 && player1.y >= treasures[i].y - 10){
       player2.speed = 0;
-      test123 = 'green'
+      // test123 = 'green'
+      // freezes = true
+      treasures.splice(i,1)
     }
     if(keyIsDown(81) == false && this.player == 1){
       player2.speed = 4;
     }
-    if(player1.x >= freeze[0].x + 10 || player1.x <= freeze[0].x -10 || player1.y >= freeze[0].y + 10 || player1.y <= freeze[0].y - 10){
+    if(player1.x >= treasures[i].x + 10 || player1.x <= treasures[i].x -10 || player1.y >= treasures[i].y + 10 || player1.y <= treasures[i].y - 10){
       if(this.player == 1){
         player2.speed = 4;
       }
     }
-    if(keyIsDown(18) == true && this.player == 2 && player2.x <= freeze[0].x + 10 && player2.x >= freeze[0].x -10 && player2.y <= freeze[0].y + 10 && player2.y >= freeze[0].y - 10){
+    if(keyIsDown(18) == true && this.player == 2 && player2.x <= treasures[i].x + 10 && player2.x >= treasures[i].x -10 && player2.y <= treasures[i].y + 10 && player2.y >= treasures[i].y - 10){
       player1.speed = 0;
-      test123 = 'green'
+      // test123 = 'green'
+      // freezes = true
+      treasures.splice(i,1)
     }
     if(keyIsDown(18) == false && this.player == 2){
       player1.speed = 4;
     }
-    if(player2.x >= freeze[0].x + 10 || player2.x <= freeze[0].x -10 || player2.y >= freeze[0].y + 10 || player2.y <= freeze[0].y - 10){
+    if(player2.x >= treasures[i].x + 10 || player2.x <= treasures[i].x -10 || player2.y >= treasures[i].y + 10 || player2.y <= treasures[i].y - 10){
       if(this.player == 2){
         player1.speed = 4;
       }
     }
+  }
 }
 }
 
 class Treasure {
-  constructor(test123){
+  constructor(){
     this.x = random(20,580);
     this.y = random(20,580);
     // this.test123 = test123
+    // this.freezes = freezes;
   }
 
   drawTreasure(){
     // stroke(this.test123)
+    stroke(255,0,0)
     strokeWeight(2)
     line(this.x - 10, this.y - 10, this.x + 10, this.y + 10)
     line(this.x - 10, this.y + 10, this.x + 10, this.y - 10)
   }
+
+  // eraseTreasure(){
+  //   if(this.freezes = freezes){
+  //     treasures.splice(special,1)
+  //   }
+  // }
 }
